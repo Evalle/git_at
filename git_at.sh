739705106e849a89e9b2b216c1c7b35aca895a4c
evalle <shmarnev@gmail.com>
2015-11-20 14:54:30 +0100

    Add some tests
    Signed-off-by: evalle <shmarnev@gmail.com>

--- git_at.sh
+++ git_at.sh
@@ -7,7 +7,7 @@
 #: Options      : none
 ############################################
 # There are four important things:
-# 1) you need to be a root to run this script;
+# 1) you need to be root to run this script;
 # 2) this script is for basic testing only!;
 # 3) you can find all your results in the .log file (see $LOG variable);
 # 4) have a lot of fun!
@@ -28,90 +28,33 @@ check() {
     fi
 }
 
-
 echo "gitat.sh is now testing git on your system, you can find all results in '$LOG' file. Please, be patient..."
 
 echo "git testing on '$HOSTNAME' host" > $LOG
 echo "" >> $LOG
 
 echo "" >> $LOG
+echo "$HOSTNAME:~ # git --version " >> $LOG
+git --version >> $LOG
+echo "test #1 Trying to check git version "
+check
+
+echo "" >> $LOG
 echo "$HOSTNAME:~ # git clone https://github.com/Evalle/pbp && cd pbp " >> $LOG
 git clone https://github.com/Evalle/pbp && cd pbp >> $LOG
-echo "test #1 Trying to clone repo"
+echo "test #2 Trying to clone repo"
 check
 
 echo "" >> $LOG
-echo "$HOSTNAME:~ # systemctl status docker" >> $LOG
-systemctl status docker >> $LOG
-echo "test #2 Check status of Docker on your system..."
-check
-
-sleep 2
-echo "" >> $LOG
-echo "$HOSTNAME:~ # systemctl restart docker" >> $LOG
-systemctl restart docker >> $LOG
-echo "test #3 Check that we can restart Docker on your system..."
+echo "$HOSTNAME:~ # git status" >> $LOG
+git status >> $LOG
+echo "test #3 Check git status..."
 check
 
 echo "" >> $LOG
-echo "$HOSTNAME:~ # docker --version" >> $LOG
-docker --version >> $LOG
-echo "test #4 Check Docker version..."
-check_version "$@"
-
-echo "" >> $LOG
-echo "$HOSTNAME:~ # ip a s | grep -i docker" >> $LOG
-ip a s | grep -i docker >> $LOG
-echo "test #5 Check that we have docker network interface on your system..."
-check 
-
-echo "" >> $LOG
-echo "$HOSTNAME:~ # docker ps" >> $LOG
-docker ps >> $LOG
-echo "test #6 Check the list of running docker containers..."
-check
-sleep 1
-
-echo "" >> $LOG
-echo "$HOSTNAME:~ # docker run opensuse uname -r" >> $LOG
-docker run opensuse uname -r &>> $LOG
-echo "test #7 Check that we can start a new docker container via 'docker run opensuse uname -r'"
-check 
-
-echo "" >> $LOG
-echo "$HOSTNAME:~ # docker run opensuse echo 'Hello world!'" >> $LOG
-docker run opensuse echo 'Hello world!' >> $LOG
-echo "test #8 Check that we can start a new docker container via 'docker run opensuse echo 'Hello world!'"
-check 
-
-echo "" >> $LOG
-echo "$HOSTNAME:~ # docker run opensuse df -h " >> $LOG
-docker run opensuse df -h >> $LOG
-echo "test #9 Check that we can start a new docker container via 'docker run opensuse df -h'..."
-check 
-
-echo "" >> $LOG
-echo "$HOSTNAME:~ # docker run opensuse mount" >> $LOG
-docker run opensuse mount >> $LOG
-echo "test #10 Check that we can start a new docker container via 'docker run opensuse mount'..."
-check
-
-echo "" >> $LOG
-echo "$HOSTNAME:~ # docker ps -a" >> $LOG
-docker ps -a >> $LOG
-echo "test #11 Check that we can see the list of all docker containers on your system..."
-check
-
-echo "" >> $LOG
-echo "$HOSTNAME:~ # docker images " >> $LOG
-docker images >> $LOG
-echo "test #12 Check that we can see the list of all docker images on your system..."
-check
-
-echo "" >> $LOG
-echo "$HOSTNAME:~ # docker info" >> $LOG
-docker info >> $LOG
-echo "test #13 Check docker info..."
+echo "$HOSTNAME:~ # git status" >> $LOG
+git status >> $LOG
+echo "test #2 Check git status..."
 check
 
 echo ""
